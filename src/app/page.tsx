9b085e75e60ac1227de138af2fea1167003fdde3
Dan Varela <danmar.dev.ph@tripleonetech.net>
2024-01-11 02:26:36 +0800

    feat: add basic downloadable qrcode implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+'use client'
+
+import { Button } from '@nextui-org/react'
+import React from 'react'
+import { QRCodeCanvas } from 'qrcode.react'
+
 export default function Home() {
-  return <main>hello</main>
+  const downloadQrCode = () => {
+    const canvas = document.querySelector('#qrcode') as HTMLCanvasElement
+    if (!canvas) throw new Error('<canvas> not found in the DOM')
+
+    const pngUrl = canvas.toDataURL('image/png')
+    const downloadLink = document.createElement('a')
+    downloadLink.href = pngUrl
+    downloadLink.download = 'QR code.png'
+    document.body.appendChild(downloadLink)
+    downloadLink.click()
+    document.body.removeChild(downloadLink)
+  }
+
+  return (
+    <main className="h-screen flex flex-col items-center justify-center gap-2">
+      <div className="p-2 border-2">
+        <QRCodeCanvas
+          value="https://danjvarela.com"
+          includeMargin
+          id="qrcode"
+        />
+      </div>
+      <Button onClick={downloadQrCode}>Download</Button>
+    </main>
+  )
 }
